593ac81f050973d4a42e1769e5e5ded0729a8213
Idara-Sunday <ekpeneidara90@gmail.com>
2023-12-11 13:53:20 +0100

    created user entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -3,12 +3,12 @@ import { Column, PrimaryColumn } from "typeorm";
 export class User {
     @PrimaryColumn('uuid')
     id:string;
-    
+
     @Column()
     email:string;
 
     @Column()
     password:string;
-
+ 
 
 }
