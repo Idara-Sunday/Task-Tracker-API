d85cb22eea016dc932c9eeb2a0ab6ff6b37f6e0b
Idara-Sunday <ekpeneidara90@gmail.com>
2023-12-11 16:52:00 +0100

    Added user resgistration functionality

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,16 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcrypt'
 
 @Injectable()
 export class UserService {
   constructor(@InjectRepository(User) private readonly userService:Repository<User>){}
   async signUp(payload:CreateUserDto) {
+    // DESTRUCTURING THE INCOMING PAYLOAD
+    var {email,password, ...rest} =payload;
+
+     email = email.toLocaleLowerCase();
     
-    const {email, ...rest} =payload;
-    const checkUser = await this.userService.findOne({where:{email}})
+    const checkUser = await this.userService.findOne({where:{email}});
+
+    if (checkUser){
+      throw new HttpException('email has been registered with another user',401)
+    }
+
+    const hashpassword = bcrypt.hash(password,34);
+
+   const saveUser = await this.userService.save({
+      email,
+      hashpassword,
+      ...rest
+    })
+    delete saveUser.hashpassword
+
+    return {
+      saveUser
+
+    }
+
   }
 
   findAll() {
